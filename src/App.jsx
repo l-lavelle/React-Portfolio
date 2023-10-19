ee5e1255fe8a7d2f0b8125e7a5a187d18eee9deb
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-18 21:10:58 -0400

    adding routes to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import NavBar from './components/navBar/navBar';
-import About from "./components/AboutMe/About";
-import Contact from './components/Contact/Contact';
-import Portfolio from './components/Portfolio/Portfolio';
+
 
 function App() {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,35 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from './App';
+import About from "./components/AboutMe/About";
+import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+    //   errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <About />,
+        },
+        {
+          path: '/About',
+          element: <Portfolio />,
+        },
+        {
+          path: '/Blog',
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+  );
