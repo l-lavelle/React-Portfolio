ae99a8fd6fa96fa16fb2caa5ec60d917031ff8fd
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-22 12:40:38 -0400

    basic resume boilerplate

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,39 +1,53 @@
 import {useState} from 'react';
 
 function Resume(){
-    const [errorMessage, setErrorMessage] = useState('');
 
     return(
+    <>
     <div className="container">
-        <h1 class="text-center">Contact Me</h1>
-        <form>
-        <h4 class="text-left">Name</h4>
-        <input class="form-control"
-            name="name"
-            type="text"
-        />
-        <h4>Email</h4>
-        <input  
-            class="form-control"
-            name="email"
-            type="email"
-        />
-        <h4>Message</h4>
-        <textarea class="form-control"
-            name="massage"
-            type="text"
-            rows="10"
-        />
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-        </form>
-        {errorMessage && (
-        <div>
-            <p className="error-text">{errorMessage}</p>
-        </div>
-        )}
+        <p>
+        Full stack web developer with a background in the healthcare field and a passion for learning. Interested in using problem solving skills to create collaborative, accessible, and user-friendly applications. Looking to use and expand my skills to bring unique insights to the role. 
+        </p>
+        <h3>Techincal Skills</h3>
+        <p>
+        Technical Languages: HTML | CSS | JavaScript | SQL| Python
+        Skills: Agile Development | Rest API’s | jQuery | Node.js | Git | Express.js | React | Debugging | MongoDB | Jest | Architectural Design Patterns (MVC, Client-Sever) | Webpack | Vite | Responsive Applications
+
+        </p>
+        <h3>Soft Skills</h3>
+        <p>
+        Analytical Thinking| Problem Solving | Interpersonal Communication | Time Management | Adaptable | Quick Learner   
+        </p>
+        <h3>Professional Experience</h3>
+        <p>
+        Pricing and Audit Analyst – Central Transport, Warren, MI 					        03/2023- present
+        •	Analyze an average of 300 Bill of Ladings daily to determine if correct class was established and updating according to customer contract if incorrect.
+        •	When freight is reweighed checking the difference between driver weight and customer assigned weight and dimensions of freight being shipped for accuracy. 
+
+        Operational Support Specialist - ATI Physical Therapy, Roseville, MI				         09/2020—09/2022
+        •	Managed clinic referral dashboard successfully converting referrals to patients scheduled in 48-hour timeframe 
+        •	Collaborated with team to provide quality patient care instructing and monitoring exercise performance and set up/delivery of therapeutic modalities
+        •	Effectively onboarded new patients by comprehensively reviewing paperwork and explaining insurance benefits in-depth 
+
+        </p>
+        <h3>Education</h3>
+        <p>
+        Full Stack Web Development Boot Camp Certificate: Michigan State University
+A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Node.js, MySQL, MongoDB, Express, Handelbars.js, and ReactJS. 
+	
+Master of Science – Athletic Training – Western Michigan University, Michigan 				           GPA: 3.93
+Bachelor of Science – Athletic Training – Saginaw Valley State University, Michigan 			           GPA: 3.78
+
+        </p>
+        <h3>Certifications</h3>
+        <p>
+        Excel Skills for Data Analytics and Visualization – by Macquarie University on Coursera			               1/2023
+Google Data Analytics Certificate – by Google on Coursera						             10/2022
+Python for Everybody Specialization – by University of Michigan on Coursera				             10/2022
+ 
+        </p>
   </div>
+  </>
   )
 }
 
