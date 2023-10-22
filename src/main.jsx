51e5aea0c94e6f4bea81d80566cd35dc09eb5e12
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-22 12:44:58 -0400

    added JS for responsive navBar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import App from './App';
