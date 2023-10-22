2edb012fdebd706550902b996d79b847769bbf73
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-22 12:12:51 -0400

    setup about boilerplate

--- src/components/AboutMe/About.jsx
+++ src/components/AboutMe/About.jsx
@@ -1,51 +1,18 @@
-import {useState} from 'react';
-
-// validate email with helper??
-// Error messgae
-// CSS
-// Actually send email
-// Message that email has been sent 
+import "./About.css"
 
 function About(){
-    const [errorMessage, setErrorMessage] = useState('');
-
-    // const handleFormSubmit = (e) =>{
-    //     e.preventDefault();
-
-
-    // }
+  
 
     return(
-    <div className="container">
-        <h1 class="text-center">Contact Me</h1>
-        <form>
-        <h4 class="text-left">Name</h4>
-        <input class="form-control"
-            name="name"
-            type="text"
-        />
-        <h4>About</h4>
-        <input  
-            class="form-control"
-            name="email"
-            type="email"
-        />
-        <h4>About</h4>
-        <textarea class="form-control"
-            name="massage"
-            type="text"
-            rows="10"
-        />
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-        </form>
-        {errorMessage && (
-        <div>
-            <p className="error-text">{errorMessage}</p>
-        </div>
-        )}
-  </div>
+    <>
+    <h1>About Me</h1>
+    <div>
+      <p>hi my name is lauren its nice to meet you</p>
+    </div>
+    <div >
+        <img  className="profile-pic" src="/images/profilePic.jpg"></img>
+    </div>
+    </>
   )
 }
 
